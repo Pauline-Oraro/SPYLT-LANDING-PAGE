3385de31a6921dd665a612f2a248b0c405b2abb4
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-07 15:28:41 +0300

    Implenenting media queries for the video and images

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,9 +1,19 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { SplitText } from 'gsap/all'
+import { useMediaQuery } from 'react-responsive'
+
 import React from 'react'
 
 const HeroSection = () => {
+
+    const isMobile = useMediaQuery({
+        query: '(max-width: 768px)'
+    });
+
+    const isTablet = useMediaQuery({
+        query: '(max-width: 1024px)'
+    })
     useGSAP(() => {
         const titleSplit = SplitText.create('.hero-title', {type: 'chars'});
 
@@ -49,11 +59,22 @@ const HeroSection = () => {
   return (
     <section className='bg-main-bg'>
       <div className='hero-container'>
-        <img 
-        src="/images/static-img.png" 
-        alt="hero image" 
-        className='absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150'
-        />
+        {
+            isTablet ? (
+                <>
+                {isMobile && <img src="/images/hero-bg.png" alt="hero image" className='absolute bottom-40 size-full object-cover' />}
+                <img src="/images/hero-img.png" alt="hero image" className='absolute bottom-0 left-1/2 -translate-x-1/2 object-auto' />
+                </>
+            ) : (
+                <video 
+                src="/videos/hero-bg.mp4"
+                autoPlay
+                muted
+                playsInline
+                className='absolute inset-0 w-full h-full object-cover'
+                />
+            )
+        }
 
         <div className='hero-content opacity-0'>
             <div className='overflow-hidden'>
